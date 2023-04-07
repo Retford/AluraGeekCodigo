d0a14db317d80b520a609209fd3dc34419bd0ae5
Retford <retfordjb@gmail.com>
2023-04-07 10:49:52 -0500

    Layout Complete

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        colorTitulo: '#464646',
+        colorPlaceholder: '#A2A2A2',
+        colorBtn: '#2A7AE4',
+        colorMain: '#F5F5F5',
+        colorContactos: '#EAF2FD'
+      }
+    }
+  },
+  plugins: []
+}
